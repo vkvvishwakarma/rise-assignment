3c8acf1b2058883c9dd319769564a65d8aadc1ba
Vinod <vkv.vinod@gmail.com>
2022-11-13 19:42:20 +0530

    initial setup

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,9 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+
   e2e: {
+    baseUrl:"https://www.saucedemo.com/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
